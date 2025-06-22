23f7fce322b0843c7a393d88c3c9d42b1aa99b63
Grayson Gantek <grayson@inspry.com>
2025-06-22 13:28:22 -0400

    Improve logging

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ Private REST API that sends Postmark emails.
 
 ## Changelog
 
+1.0.1
+- Improve logging
+
 1.0.0
 - Initial commit
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ require('dotenv').config()
 import express from 'express'
 import { createPostmarkClient } from './lib/createPostmarkClient'
 import { env } from './lib/env'
+import { logger } from './middleware/logger'
 import sendEmail from './routes/api/email/send'
 
 const { POSTMARK_API_TOKEN, PORT } = env
 const app = express()
 
+app.use(express.json())
+app.use(logger)
+
 createPostmarkClient(POSTMARK_API_TOKEN)
 
-app.use(express.json())
 app.use('/', sendEmail)
 
 app.listen(PORT, (error) => {
