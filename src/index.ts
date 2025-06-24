63d1da261e9544814b2e5d806c6957a630a33356
Grayson Gantek <github@graysn.com>
2025-06-23 23:58:05 -0400

    Add basic error logger

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ require('dotenv').config()
 import express from 'express'
 import { createPostmarkClient } from './lib/createPostmarkClient'
 import { env } from './lib/env'
+import { errors } from './middleware/errors'
 import { logger } from './middleware/logger'
 import sendEmail from './routes/api/email/send'
 
 const { POSTMARK_API_TOKEN, PORT } = env
+
+createPostmarkClient(POSTMARK_API_TOKEN)
+
 const app = express()
 
 app.use(express.json())
 app.use(logger)
-
-createPostmarkClient(POSTMARK_API_TOKEN)
-
 app.use('/', sendEmail)
+app.use(errors)
 
 app.listen(PORT, (error) => {
   if (error) {
